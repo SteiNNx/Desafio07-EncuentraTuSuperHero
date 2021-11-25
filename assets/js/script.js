14a0d65e5fbb72b1cc3547bf2dcfa7f4d0c65aff
Jorge Reyes <jorge_irving_15@hotmail.com>
2021-11-25 20:02:54 -0300

    ADD: Style, loading y captar errores

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,11 @@ $(document).ready(function () {
         document.getElementById('searchHelp').innerHTML = valueText;
     }
 
+    const removeDisplayNone = id_element => document.getElementById(id_element).classList.remove('d-none');
+    const addDisplayNone = id_element => document.getElementById(id_element).classList.add('d-none');
+
     const setCard = valueSuperHero => {
+        document.getElementById('resultado-superhero').classList.remove('d-none');
         document.getElementById('card-avatar').src = valueSuperHero.image.url;
         document.getElementById('card-title').innerText = `Nombre: ${valueSuperHero.name}`;
         document.getElementById('card-text').innerText = `Conexiones: ${valueSuperHero.connections['group-affiliation']}`;
@@ -41,47 +45,69 @@ $(document).ready(function () {
                 }
             });
 
-        let chart = new CanvasJS.Chart("graph_container", {
-            theme: "light2",
-            exportEnabled: false,
-            animationEnabled: true,
-            title: {
-                text: `Estadísticas de Poder para ${valueSuperHero.name}`,
-                fontFamily: "Roboto",
-                fontWeight: "700",
-            },
+        const faltanDatos = statsSuperHero.some((value) => value.y == 'null');
+        if (!faltanDatos) {
+            let chart = new CanvasJS.Chart("graph_container", {
+                theme: "light2",
+                exportEnabled: false,
+                animationEnabled: true,
+                title: {
+                    text: `Estadísticas de Poder para ${valueSuperHero.name}`,
+                    fontFamily: "Roboto",
+                    fontWeight: "700",
+                },
 
-            data: [{
-                type: "pie",
-                startAngle: 25,
-                toolTipContent: "<b>{label}</b>: {y}%",
-                showInLegend: "true",
-                legendText: "{label}",
-                indexLabelFontFamily: "Roboto",
-                indexLabelFontWeight: "500",
-                indexLabelFontSize: 16,
-                indexLabel: "{label} ({y})",
-                dataPoints: statsSuperHero,
-            }]
-        });
-        chart.render();
+                data: [{
+                    type: "pie",
+                    startAngle: 25,
+                    toolTipContent: "<b>{label}</b>: {y}%",
+                    showInLegend: "true",
+                    legendText: "{label}",
+                    indexLabelFontFamily: "Roboto",
+                    indexLabelFontWeight: "500",
+                    indexLabelFontSize: 16,
+                    indexLabel: "{label} ({y})",
+                    dataPoints: statsSuperHero,
+                }]
+            });
+            chart.render();
+        } else {
+            document.getElementById('graph_container').innerHTML = '<p>Datos insuficientes...</p>'
+        }
     }
-
 
 
     $("#btn-buscar-superhero").click((event) => {
         event.preventDefault();
+
+        /** Inicio Efecto Loading */
+        removeDisplayNone('buscar-loading');
+        addDisplayNone('buscar-label');
+
+        /** Limpio label help */
         setHelp('');
+
+        /** Obtengo value del input search */
         const inputValue = obtenerValueInput();
+
         $.ajax({
             url: `${API_URL}/${inputValue}`,
         })
             .done((success) => {
-                setCard(success);
-                setGraph(success);
+                if (success.response == 'success') {
+                    setCard(success);
+                    setGraph(success);
+                } else {
+                    setHelp('Error Api SuperHero!');
+                }
             })
             .fail((error) => {
                 console.error(error);
+            })
+            .always(() => {
+                /** Termino Efecto Loading */
+                addDisplayNone('buscar-loading');
+                removeDisplayNone('buscar-label');
             });
     });
 
